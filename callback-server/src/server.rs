559638d6164740cfc4c175fca28d5af59424b70d
Tristan Timblin <tatimblin@gmail.com>
2026-05-02 16:45:31 -0700

    remove print

--- callback-server/src/server.rs
+++ callback-server/src/server.rs
@@ -104,9 +104,7 @@ impl CallbackServer {
         let local_ip = Self::detect_local_ip()
             .ok_or_else(|| "Failed to detect local IP address".to_string())?;
 
-        eprintln!("Detected local IP address: {local_ip}");
         let base_url = format!("http://{local_ip}:{port}");
-        eprintln!("Callback server base URL: {base_url}");
 
         // Create event router
         let event_router = Arc::new(EventRouter::new(event_sender));
